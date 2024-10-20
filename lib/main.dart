41a45f88ae147ee6e695052e3b56bb8448ded83b
yasser-osamaa <yasserosamaa88@gmail.com>
2024-10-20 23:26:14 +0300

    adding poppins font

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flighter/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,9 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Placeholder(),
+      theme: ThemeData(
+        fontFamily: 'Poppins-Regular',
+      ),
+      home: const SplashView(),
     );
   }
 }
