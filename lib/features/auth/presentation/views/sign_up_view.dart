327a08afe15c6244393d5360ef7d0f623d039184
yasser-osamaa <yasserosamaa88@gmail.com>
2024-10-21 22:11:36 +0300

    add sign up view

---
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -0,0 +1,15 @@
+import 'package:flighter/features/auth/presentation/views/widgets/sign_up_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SignUpView extends StatelessWidget {
+  const SignUpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: SignUpViewBody(),
+      ),
+    );
+  }
+}
