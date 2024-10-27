3d664b1818d726cb5f695a3766807b2ff76a38fe
yasser-osamaa <yasserosamaa88@gmail.com>
2024-10-28 01:02:52 +0300

    set new password validate is done

--- lib/features/auth/presentation/views/widgets/set_new_password_view_body.dart
+++ lib/features/auth/presentation/views/widgets/set_new_password_view_body.dart
@@ -15,52 +15,58 @@ class SetNewPasswordBody extends StatefulWidget {
 }
 
 class _SetNewPasswordBodyState extends State<SetNewPasswordBody> {
+  final key = GlobalKey<FormState>();
   bool disable = true;
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: Column(
-          children: [
-            Text(
-              'Set a new password ',
-              style: Styles.textStyle35,
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            Text(
-              'Create a new password. Ensure it differs from previous ones for security',
-              style: Styles.textStyle16
-                  .copyWith(color: kGreyColor.withOpacity(0.6)),
-            ),
-            SizedBox(
-              height: 40.h,
-            ),
-            const PasswordTextFormField(
-              text: 'Password',
-            ),
-            SizedBox(
-              height: 25.h,
-            ),
-            const PasswordTextFormField(text: 'Confirm Password'),
-            SizedBox(
-              height: 50.h,
-            ),
-            CustomButton(
-              text: 'Update Password',
-              onPressed: () {
-                setState(() {
-                  successDialog(context);
-                });
-              },
-              height: 73,
-            ),
-            SizedBox(
-              height: 30.h,
-            ),
-          ],
+        child: Form(
+          key: key,
+          child: Column(
+            children: [
+              Text(
+                'Set a new password ',
+                style: Styles.textStyle35,
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Text(
+                'Create a new password. Ensure it differs from previous ones for security',
+                style: Styles.textStyle16
+                    .copyWith(color: kGreyColor.withOpacity(0.6)),
+              ),
+              SizedBox(
+                height: 40.h,
+              ),
+              const PasswordTextFormField(
+                text: 'Password',
+              ),
+              SizedBox(
+                height: 25.h,
+              ),
+              const PasswordTextFormField(text: 'Confirm Password'),
+              SizedBox(
+                height: 50.h,
+              ),
+              CustomButton(
+                text: 'Update Password',
+                onPressed: () {
+                  if (key.currentState!.validate()) {
+                    setState(() {
+                      successDialog(context);
+                    });
+                  }
+                },
+                height: 73,
+              ),
+              SizedBox(
+                height: 30.h,
+              ),
+            ],
+          ),
         ),
       ),
     );
