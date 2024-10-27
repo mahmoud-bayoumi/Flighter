526de18b318c4362a91bdfbc3d932c4a691d5281
yasser-osamaa <yasserosamaa88@gmail.com>
2024-10-28 00:03:18 +0300

    add validate key to sign up view

--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -10,75 +10,89 @@ import 'package:go_router/go_router.dart';
 
 import '../../../../../core/utils/styles.dart';
 
-class SignUpViewBody extends StatelessWidget {
+class SignUpViewBody extends StatefulWidget {
   const SignUpViewBody({super.key});
+
+  @override
+  State<SignUpViewBody> createState() => _SignUpViewBodyState();
+}
+
+class _SignUpViewBodyState extends State<SignUpViewBody> {
+  final key = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 10.h,
-            ),
-            Text(
-              'Create your account',
-              style: Styles.textStyle35,
-            ),
-            SizedBox(
-              height: 25.h,
-            ),
-            const CustomTextFormField(text: 'Name', hintText: 'ex: jon smith'),
-            SizedBox(
-              height: 14.h,
-            ),
-            const CustomTextFormField(
-                text: 'Email', hintText: 'ex: jon.smith@email.com'),
-            SizedBox(
-              height: 14.h,
-            ),
-            const PasswordTextFormField(
-              text: 'Password',
-            ),
-            SizedBox(
-              height: 14.h,
-            ),
-            const PasswordTextFormField(
-              text: 'Confirm Password',
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            const CustomButton(
-              text: 'SIGN UP',
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            Text(
-              'or sign up with',
-              style: Styles.textStyle16.copyWith(color: kGreyColor),
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            const SocialAuthButtons(),
-            SizedBox(
-              height: 15.h,
-            ),
-            AuthTextButton(
-              authDesc: 'Have an account?',
-              authButtonName: 'SIGN IN',
-              onPressed: () => GoRouter.of(context).pop(),
-            ),
-            SizedBox(
-              height: 15.h,
-            ),
-          ],
+        child: Form(
+          key: key,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 10.h,
+              ),
+              Text(
+                'Create your account',
+                style: Styles.textStyle35,
+              ),
+              SizedBox(
+                height: 25.h,
+              ),
+              const CustomTextFormField(
+                  text: 'Name', hintText: 'ex: jon smith'),
+              SizedBox(
+                height: 14.h,
+              ),
+              const CustomTextFormField(
+                  text: 'Email', hintText: 'ex: jon.smith@email.com'),
+              SizedBox(
+                height: 14.h,
+              ),
+              const PasswordTextFormField(
+                text: 'Password',
+              ),
+              SizedBox(
+                height: 14.h,
+              ),
+              const PasswordTextFormField(
+                text: 'Confirm Password',
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              CustomButton(
+                text: 'SIGN UP',
+                onPressed: () {
+                  if (key.currentState!.validate()) {}
+                },
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Text(
+                'or sign up with',
+                style: Styles.textStyle16.copyWith(color: kGreyColor),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              const SocialAuthButtons(),
+              SizedBox(
+                height: 15.h,
+              ),
+              AuthTextButton(
+                authDesc: 'Have an account?',
+                authButtonName: 'SIGN IN',
+                onPressed: () => GoRouter.of(context).pop(),
+              ),
+              SizedBox(
+                height: 15.h,
+              ),
+            ],
+          ),
         ),
       ),
     );
