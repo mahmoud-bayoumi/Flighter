78c56e65897933ab706d64c6f0500002b36708e5
yasser-osamaa <yasserosamaa88@gmail.com>
2024-11-25 14:21:35 +0200

    merge ticket widget and add app router to go to choose seat

--- lib/features/home/presentation/views/widgets/choose_seat_widgets/choose_seat_view_body.dart
+++ lib/features/home/presentation/views/widgets/choose_seat_widgets/choose_seat_view_body.dart
@@ -1,7 +1,6 @@
 import 'package:flighter/constants.dart';
+import 'package:flighter/features/home/presentation/views/widgets/choose_seat_widgets/selection_status_row.dart';
 import 'package:flutter/material.dart';
-
-import 'selection_status_row.dart';
 
 class ChooseSeatViewBody extends StatelessWidget {
   const ChooseSeatViewBody({super.key});
@@ -10,8 +9,19 @@ class ChooseSeatViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        Row(),
-    
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SelectionStatusRow(
+              selectionColor: kPrimaryColor,
+              selectionText: 'Selected',
+            ),
+            SelectionStatusRow(
+              selectionColor: Color(0xff7C7270),
+              selectionText: 'Reserved',
+            ),
+          ],
+        )
       ],
     );
   }
