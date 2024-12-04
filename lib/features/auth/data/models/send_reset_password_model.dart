95a687010238e9fd8098becc9f9cee0a6c9ed6ea
Mahmoud Mohamed <mahmoudbayoumi110@gmail.com>
2024-12-04 16:32:07 +0200

    handle send reset password code api

---
+++ lib/features/auth/data/models/send_reset_password_model.dart
@@ -0,0 +1,8 @@
+class SendResetPasswordModel {
+  String? message;
+  SendResetPasswordModel({required this.message});
+  
+  factory SendResetPasswordModel.fromJson(json) {
+    return SendResetPasswordModel(message: json['message']);
+  }
+}
