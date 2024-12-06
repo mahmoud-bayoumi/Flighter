d1a2483e58d825109182a9cf1b2412855c4e99e5
yasser-osamaa <yasserosamaa88@gmail.com>
2024-12-06 18:22:40 +0200

    handle home get profile data by bloc builder

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,8 +1,8 @@
 import 'dart:developer';
-
 import 'package:flighter/constants.dart';
 import 'package:flighter/core/utils/assets_data.dart';
 import 'package:flighter/core/utils/styles.dart';
+import 'package:flighter/core/widgets/failure_page_widget.dart';
 import 'package:flighter/core/widgets/primary_container.dart';
 import 'package:flighter/features/home/presentation/views/widgets/hot_deals_list_view.dart';
 import 'package:flighter/features/home/presentation/views/widgets/search_container.dart';
@@ -10,7 +10,6 @@ import 'package:flighter/features/profile/presentation/view_model/get_profile_da
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../../../../core/utils/functions/capitalize_word.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -23,70 +22,84 @@ class HomeViewBody extends StatelessWidget {
     String userName = getData.profileData!.name!;
     log('USER NAME : ${getData.profileData!.name!}');
 
-    return SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      child: Stack(
-        children: [
-          SizedBox(
-            height: MediaQuery.sizeOf(context).height,
-          ),
-          const PrimaryContainer(),
-          Padding(
-            padding: EdgeInsets.only(top: 45.h, left: 10.w),
-            child: Text(
-              'Hello, ${capitalizeFirstLetter(userName)}',
-              style: Styles.textStyle24.copyWith(color: Colors.white),
+    return BlocBuilder<GetProfileDataCubit, GetProfileDataState>(
+      builder: (context, state) {
+        if (state is GetProfileDataLoading) {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: kPrimaryColor,
             ),
-          ),
-          Positioned(
-            left: MediaQuery.sizeOf(context).width * 0.69,
-            child: SizedBox(
-              height: 270.h,
-              child: AspectRatio(
-                aspectRatio: 3 / 4,
-                child: Image.asset(
-                  AssetsData.kReversedLogo,
+          );
+        } else if (state is GetProfileDataFailure) {
+          return const FailurePageWidget();
+        } else {
+          return SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height,
                 ),
-              ),
+                const PrimaryContainer(),
+                Padding(
+                  padding: EdgeInsets.only(top: 45.h, left: 10.w),
+                  child: Text(
+                    'Hello, ${capitalizeFirstLetter(userName)}',
+                    style: Styles.textStyle24.copyWith(color: Colors.white),
+                  ),
+                ),
+                Positioned(
+                  left: MediaQuery.sizeOf(context).width * 0.69,
+                  child: SizedBox(
+                    height: 270.h,
+                    child: AspectRatio(
+                      aspectRatio: 3 / 4,
+                      child: Image.asset(
+                        AssetsData.kReversedLogo,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: MediaQuery.sizeOf(context).height * .2,
+                  left: MediaQuery.sizeOf(context).height * .035,
+                  child: Text(
+                    'Book Your Flight',
+                    style: Styles.textStyle45,
+                  ),
+                ),
+                Positioned(
+                  top: MediaQuery.sizeOf(context).height * .3,
+                  left: MediaQuery.sizeOf(context).width * .02,
+                  right: MediaQuery.sizeOf(context).width * .02,
+                  child: Container(
+                    width: MediaQuery.sizeOf(context).width * 1,
+                    height: MediaQuery.sizeOf(context).height,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
+                    child: const SearchContainer(),
+                  ),
+                ),
+                Positioned(
+                  top: MediaQuery.sizeOf(context).height * .86,
+                  left: MediaQuery.sizeOf(context).width * .04,
+                  child: Text(
+                    'Hot Deals',
+                    style: Styles.textStyle35.copyWith(color: kPrimaryColor),
+                  ),
+                ),
+                Positioned.fill(
+                  top: MediaQuery.sizeOf(context).height * 0.92,
+                  left: MediaQuery.sizeOf(context).width * .02,
+                  child: const HotDealsListView(),
+                ),
+              ],
             ),
-          ),
-          Positioned(
-            top: MediaQuery.sizeOf(context).height * .2,
-            left: MediaQuery.sizeOf(context).height * .035,
-            child: Text(
-              'Book Your Flight',
-              style: Styles.textStyle45,
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.sizeOf(context).height * .3,
-            left: MediaQuery.sizeOf(context).width * .02,
-            right: MediaQuery.sizeOf(context).width * .02,
-            child: Container(
-              width: MediaQuery.sizeOf(context).width * 1,
-              height: MediaQuery.sizeOf(context).height,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.white,
-              ),
-              child: const SearchContainer(),
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.sizeOf(context).height * .86,
-            left: MediaQuery.sizeOf(context).width * .04,
-            child: Text(
-              'Hot Deals',
-              style: Styles.textStyle35.copyWith(color: kPrimaryColor),
-            ),
-          ),
-          Positioned.fill(
-            top: MediaQuery.sizeOf(context).height * 0.92,
-            left: MediaQuery.sizeOf(context).width * .02,
-            child: const HotDealsListView(),
-          ),
-        ],
-      ),
+          );
+        }
+      },
     );
   }
 }
