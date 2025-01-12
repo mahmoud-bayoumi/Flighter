4610622608db8e0cd0566b9dd4794bb9cc1f83eb
Mahmoud Mohamed <mahmoudbayoumi110@gmail.com>
2025-01-12 20:34:03 +0200

    fix the overflow of the user name

--- lib/features/profile/presentation/views/widgets/edit_profile_widgets/edit_profile_view_body.dart
+++ lib/features/profile/presentation/views/widgets/edit_profile_widgets/edit_profile_view_body.dart
@@ -62,6 +62,7 @@ class EditProfileViewBody extends StatelessWidget {
                       onSaved: (newValue) {
                         updateCubit.name.text = newValue!;
                       },
+                     
                       controller: updateCubit.name,
                       decoration: InputDecoration(
                         hintText: 'Enter your name here',
