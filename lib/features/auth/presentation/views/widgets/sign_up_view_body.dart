1d876f1bf36de2939afcefe3fa8674b00ab4f33e
yasser-osamaa <yasserosamaa88@gmail.com>
2025-02-26 18:20:36 +0200

    sign up handle internet connection

--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+import 'dart:developer';
 import 'package:flighter/constants.dart';
 import 'package:flighter/core/utils/app_router.dart';
 import 'package:flighter/core/utils/functions/dialogs_type.dart';
 import 'package:flighter/core/widgets/custom_button.dart';
 import 'package:flighter/core/widgets/custom_text_form_field.dart';
+import 'package:flighter/core/widgets/no_internet_connection_view.dart';
 import 'package:flighter/core/widgets/password_text_form_field.dart';
 import 'package:flighter/features/auth/presentation/view_model/sign_up_cubit/sign_up_cubit.dart';
 import 'package:flighter/features/auth/presentation/views/widgets/auth_text_button.dart';
@@ -11,6 +14,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import '../../../../../core/utils/styles.dart';
 
 class SignUpViewBody extends StatefulWidget {
@@ -21,99 +25,139 @@ class SignUpViewBody extends StatefulWidget {
 }
 
 class _SignUpViewBodyState extends State<SignUpViewBody> {
+  bool isConnectedToInternet = false;
+  StreamSubscription? _internetConnectionStreamSubscription;
+
+  @override
+  void initState() {
+    _internetConnectionStreamSubscription =
+        InternetConnection().onStatusChange.listen(
+      (event) {
+        log(event.toString());
+        switch (event) {
+          case InternetStatus.connected:
+            setState(() {
+              isConnectedToInternet = true;
+            });
+            break;
+          case InternetStatus.disconnected:
+            setState(() {
+              isConnectedToInternet = false;
+            });
+
+            break;
+          default:
+            setState(() {
+              isConnectedToInternet = false;
+            });
+            break;
+        }
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _internetConnectionStreamSubscription?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     var cubitData = context.read<SignUpCubit>();
-    return BlocConsumer<SignUpCubit, SignUpState>(
-      listener: (context, state) {
-        if (state is SignUpSuccess) {
-          EasyLoading.dismiss();
-          GoRouter.of(context).push(AppRouter.kCheckYourEmailView,
-              extra: cubitData.emailController.text);
-        } else if (state is SignUpFailure) {
-          EasyLoading.dismiss();
-          errorDialog(context, state.errMsg);
-        } else if (state is SignUpLoading) {
-          EasyLoading.show(status: 'loading...');
-        }
-      },
-      builder: (context, state) {
-        return SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w),
-            child: Form(
-              key: cubitData.formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  Text(
-                    'Create your account',
-                    style: Styles.textStyle35,
-                  ),
-                  SizedBox(
-                    height: 25.h,
-                  ),
-                  CustomTextFormField(
-                    text: 'Name',
-                    hintText: 'ex: jon smith',
-                    textEditingController: cubitData.nameController,
-                  ),
-                  SizedBox(
-                    height: 14.h,
-                  ),
-                  CustomTextFormField(
-                    text: 'Email',
-                    hintText: 'ex:jon_smith@email.com',
-                    textEditingController: cubitData.emailController,
-                  ),
-                  SizedBox(
-                    height: 14.h,
-                  ),
-                  PasswordTextFormField(
-                    text: 'Password',
-                    controller: cubitData.passwordController,
-                  ),
-                  SizedBox(
-                    height: 14.h,
-                  ),
-                  PasswordTextFormField(
-                    controller: cubitData.confirmPasswordController,
-                    text: 'Confirm Password',
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  state is SignUpLoading
-                      ? const CircularProgressIndicator(
-                          color: kPrimaryColor,
-                        )
-                      : CustomButton(
-                          text: 'SIGN UP',
-                          onPressed: () {
-                            cubitData.validateUser();
-                          },
+    return isConnectedToInternet
+        ? BlocConsumer<SignUpCubit, SignUpState>(
+            listener: (context, state) {
+              if (state is SignUpSuccess) {
+                EasyLoading.dismiss();
+                GoRouter.of(context).push(AppRouter.kCheckYourEmailView,
+                    extra: cubitData.emailController.text);
+              } else if (state is SignUpFailure) {
+                EasyLoading.dismiss();
+                errorDialog(context, state.errMsg);
+              } else if (state is SignUpLoading) {
+                EasyLoading.show(status: 'loading...');
+              }
+            },
+            builder: (context, state) {
+              return SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w),
+                  child: Form(
+                    key: cubitData.formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 10.h,
                         ),
-                  SizedBox(
-                    height: 40.h,
+                        Text(
+                          'Create your account',
+                          style: Styles.textStyle35,
+                        ),
+                        SizedBox(
+                          height: 25.h,
+                        ),
+                        CustomTextFormField(
+                          text: 'Name',
+                          hintText: 'ex: jon smith',
+                          textEditingController: cubitData.nameController,
+                        ),
+                        SizedBox(
+                          height: 14.h,
+                        ),
+                        CustomTextFormField(
+                          text: 'Email',
+                          hintText: 'ex:jon_smith@email.com',
+                          textEditingController: cubitData.emailController,
+                        ),
+                        SizedBox(
+                          height: 14.h,
+                        ),
+                        PasswordTextFormField(
+                          text: 'Password',
+                          controller: cubitData.passwordController,
+                        ),
+                        SizedBox(
+                          height: 14.h,
+                        ),
+                        PasswordTextFormField(
+                          controller: cubitData.confirmPasswordController,
+                          text: 'Confirm Password',
+                        ),
+                        SizedBox(
+                          height: 20.h,
+                        ),
+                        state is SignUpLoading
+                            ? const CircularProgressIndicator(
+                                color: kPrimaryColor,
+                              )
+                            : CustomButton(
+                                text: 'SIGN UP',
+                                onPressed: () {
+                                  cubitData.validateUser();
+                                },
+                              ),
+                        SizedBox(
+                          height: 40.h,
+                        ),
+                        AuthTextButton(
+                          authDesc: 'Have an account?',
+                          authButtonName: 'SIGN IN',
+                          onPressed: () => GoRouter.of(context).pop(),
+                        ),
+                        SizedBox(
+                          height: 15.h,
+                        ),
+                      ],
+                    ),
                   ),
-                  AuthTextButton(
-                    authDesc: 'Have an account?',
-                    authButtonName: 'SIGN IN',
-                    onPressed: () => GoRouter.of(context).pop(),
-                  ),
-                  SizedBox(
-                    height: 15.h,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
+                ),
+              );
+            },
+          )
+        : const NoInternetConnectionView();
   }
 }
